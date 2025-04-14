d713c05ae31cedfe2d523c5abad85e7d12739f28
Jose matheus <j.matheus.v.souza@gmail.com>
2025-04-14 08:46:07 -0300

    inicio de while

---
+++ exercicio02.py
@@ -0,0 +1,21 @@
+# n = 10
+# soma = 0
+# i = 1
+#
+# while i <= n:
+#     soma+=1
+#     i = i+1
+#
+# print("a soma é: ", soma)
+
+nota = 0
+x = 0
+while x <= 5:
+    num=int(input("informe o primeiro numero"))
+    nota = nota+num
+    nota += 1
+media=nota/5
+print(media)
+
+
+
